01a5a909875a2441fc2eab00f48a2b891567dc4d
pollockcl <pollock.clee@gmail.com>
2018-02-16 16:18:00 -0700

    some hound changes

--- test/sales_engine_test.rb
+++ test/sales_engine_test.rb
@@ -1,9 +1,7 @@
 # Tests for the SalesEngine class
 require './test/test_helper'
 require './lib/sales_engine'
-
 class SalesEngineTest < MiniTest::Test
-
   def test_existence
     se = SalesEngine.new
 
