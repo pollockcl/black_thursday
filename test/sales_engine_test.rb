19bc3bdc6180ba44c0f462ec128afe4c62c2e606
pollockcl <pollock.clee@gmail.com>
2018-02-16 16:32:07 -0700

    changed test nature and added test

--- test/sales_engine_test.rb
+++ test/sales_engine_test.rb
@@ -2,9 +2,22 @@
 require './test/test_helper'
 require './lib/sales_engine'
 class SalesEngineTest < MiniTest::Test
-  def test_existence
-    se = SalesEngine.new
+  def setup
+    @files = {:items     => "./data/items.csv",
+              :merchants => "./data/merchants.csv"}
+
+  end
+  
+  def test_from_csv_method_and_existance
+    se = SalesEngine.from_csv(@files)
 
     assert_instance_of SalesEngine, se
+  end
+
+  def test_has_merch_and_items
+    se = SalesEngine.from_csv(@files)
+    
+    assert_instance_of ItemRepo, se.items
+    assert_instance_of MerchRepo, se.merchants
   end
 end
