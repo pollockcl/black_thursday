2f8100e8cd62d526cd50cb229c29d9f64b45bdca
pollockcl <pollock.clee@gmail.com>
2018-02-16 18:21:49 -0700

    test fixes

--- test/item_test.rb
+++ test/item_test.rb
@@ -1,15 +1,16 @@
 require_relative './test_helper'
 require './lib/item.rb'
+require 'bigdecimal'
 
-class ItemTest < Test::MiniTest
+class ItemTest < MiniTest::Test
   def test_existence
-    item = Item.new("Pencil", "You can use it to write things", BigDecimal.new(10.99,4), Time.now, Time.now})
+    item = Item.new("Pencil", "You can use it to write things", BigDecimal.new(10.99,4), Time.now, Time.now)
 
     assert_instance_of Item, item
   end
 
   def test_attributes
-    item = Item.new("Pencil", "You can use it to write things", BigDecimal.new(10.99,4), Time.now, Time.now})
+    item = Item.new("Pencil", "You can use it to write things", BigDecimal.new(10.99,4), Time.now, Time.now)
 
     assert_equal 'Pencil', item.name
     assert_equal 'You can use it to write things', item.description
