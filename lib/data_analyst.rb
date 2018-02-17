76b850631733b3649993247a31fed315a67e9097
pollockcl <pollock.clee@gmail.com>
2018-02-17 10:02:26 -0700

    added parsing for the items to get id and merchant_id

--- lib/data_analyst.rb
+++ lib/data_analyst.rb
@@ -10,11 +10,13 @@ class DataAnalyst
 
   def self.find_items(file)
     csv = CSV.read(file, :headers => true)
+    id          = csv['id']
     name        = csv['name']
     description = csv['description']
     unit_price  = csv['unit_price']
+    merchant_id = csv['merchant_id']
     created_at  = csv['created_at']
     updated_at  = csv['updated_at']
-    [name, description, unit_price, created_at, updated_at].transpose
+    [id, name, description, unit_price, merchant_id, created_at, updated_at].transpose
   end
 end
