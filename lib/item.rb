817f8728ac5749461a6d159118c99407d6e66b77
pollockcl <pollock.clee@gmail.com>
2018-02-16 18:22:05 -0700

    added attr_reader and removed some attributes

--- lib/item.rb
+++ lib/item.rb
@@ -1,14 +1,12 @@
 # This is the Item class
 class Item
-
-  def initialize(id, name, description, unit_price, created_at, updated_at, merchant_id)
-    @id          = id
+  attr_reader :name, :description, :unit_price, :created_at, :updated_at
+  def initialize(name, description, unit_price, created_at, updated_at)
     @name        = name
     @description = description
     @unit_price  = unit_price
     @created_at  = created_at
     @updated_at  = updated_at
-    @merchant_id = merchant_id
   end
 
 end
