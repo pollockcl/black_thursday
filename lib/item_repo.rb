eebbfcd176967256d83f2830fba07fac4ce3c0ee
pollockcl <pollock.clee@gmail.com>
2018-02-16 18:41:50 -0700

    added attr_reader and logic to add items

--- lib/item_repo.rb
+++ lib/item_repo.rb
@@ -1,6 +1,12 @@
 # This is the ItemRepo class
+require_relative 'item'
+require_relative 'data_analyst'
 class ItemRepo
-  def initialize
-    
+  attr_reader :items
+  def initialize(csv)
+    @items = []
+    DataAnalyst.find_items(csv).each do |item|
+      @items << Item.new(item[0], item[1], item[2], item[3], item[4])
+    end
   end
 end
