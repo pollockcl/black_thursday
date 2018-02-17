cdf57115ffa2ba1159599375d7da2d518cc04bf3
pollockcl <pollock.clee@gmail.com>
2018-02-16 18:43:10 -0700

    add test for find_items method

--- test/data_analyst_test.rb
+++ test/data_analyst_test.rb
@@ -2,8 +2,14 @@ require_relative 'test_helper'
 require './lib/data_analyst'
 class DataAnalystTest < MiniTest::Test
   def test_find_merchants
-    file = './data/merchants.csv' 
+    file = './data/merchants.csv'  
 
     assert_instance_of Array, DataAnalyst.find_merchants(file)
+  end
+
+  def test_find_items
+    file = './data/items.csv'
+
+    assert_instance_of Array, DataAnalyst.find_items(file)
   end
 end
