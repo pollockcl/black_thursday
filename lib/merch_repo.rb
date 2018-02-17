64fbe4383b5d97fee0a35332f467037650605f0d
pollockcl <pollock.clee@gmail.com>
2018-02-16 18:07:40 -0700

    added some logic to create all the merchants from a csv file

--- lib/merch_repo.rb
+++ lib/merch_repo.rb
@@ -1,8 +1,13 @@
 # This is the MerchRepo class
 require 'csv'
+require_relative './data_analyst'
+require_relative './merchant'
 class MerchRepo
+  attr_reader :merchants
   def initialize(csv)
-    @merchants = Hash.new
-    
+    @merchants = []
+    DataAnalyst.find_merchants(csv).each do |item|
+      @merchants << Merchant.new(item[0], item[1])
+    end
   end
 end
