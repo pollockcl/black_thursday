03948f69fad01c121b30b3e561c8862001e453a2
pollockcl <pollock.clee@gmail.com>
2018-02-16 17:12:52 -0700

    slight changes in design

--- test/data_analyst_test.rb
+++ test/data_analyst_test.rb
@@ -1,10 +1,9 @@
 require_relative 'test_helper'
+require './lib/data_analyst'
 class DataAnalystTest < MiniTest::Test
+  def test_find_merchants
+    file = './data/merchants.csv' 
 
-  def test_existence
-    da = DataAnalyst.new
-
-    assert_instance_of DataAnalyst, da
+    assert_instance_of Array, DataAnalyst.find_merchants(file)
   end
-
 end
