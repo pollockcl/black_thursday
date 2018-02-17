a6c01fcc33a65c4be70e83de998fb5c4087b65f7
pollockcl <pollock.clee@gmail.com>
2018-02-16 18:49:51 -0700

    changed var names and added attr_reader

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -1,8 +1,15 @@
 # The SalesEngine class
+require_relative './merch_repo'
+require_relative './item_repo'
 class SalesEngine
+  attr_reader :merchant_repo, :item_repo
+  def self.from_csv(hash)
+    SalesEngine.new(hash)
+  end
 
-  def initialize
-
+  def initialize(hash)
+    @merchant_repo = MerchRepo.new(hash[:merchants])
+    @item_repo     = ItemRepo.new(hash[:items])
   end
 
 end
