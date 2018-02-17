5f77d71fbad3af89256050f92c2b1f1a463cae04
pollockcl <pollock.clee@gmail.com>
2018-02-16 18:42:12 -0700

    added logic to find items through csv file

--- lib/data_analyst.rb
+++ lib/data_analyst.rb
@@ -7,4 +7,14 @@ class DataAnalyst
     names = csv['name']
     ids.zip(names)
   end
+
+  def self.find_items(file)
+    csv = CSV.read(file, :headers => true)
+    name        = csv['name']
+    description = csv['description']
+    unit_price  = csv['unit_price']
+    created_at  = csv['created_at']
+    updated_at  = csv['updated_at']
+    [name, description, unit_price, created_at, updated_at].transpose
+  end
 end
