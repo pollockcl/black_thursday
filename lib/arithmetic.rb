6d1d45471e907145dcbfa27886ff9ddfb2dde007
pollockcl <pollock.clee@gmail.com>
2018-02-18 19:18:58 -0700

    added standard_deviation method

--- lib/arithmetic.rb
+++ lib/arithmetic.rb
@@ -4,4 +4,9 @@ module Arithmetic
   def average(numerator, denominator)
     (BigDecimal(numerator) / BigDecimal(denominator)).round(2)
   end
+
+  def standard_deviation(range, average)
+    Math.sqrt(range.map do |item| (item.items.size - average)**2
+    end.sum / range.size).round(2)
+  end
 end
