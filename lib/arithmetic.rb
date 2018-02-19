fa409686c12f8afd13c1bd9eeb7abd70b1eb3a43
pollockcl <pollock.clee@gmail.com>
2018-02-18 19:57:19 -0700

    slight changes

--- lib/arithmetic.rb
+++ lib/arithmetic.rb
@@ -6,7 +6,8 @@ module Arithmetic
   end
 
   def standard_deviation(range, average)
-    Math.sqrt(range.map do |item| (item.items.size - average)**2
+    Math.sqrt(range.map do |item|
+      (item.items.size - average)**2
     end.sum / range.size).round(2)
   end
 end
