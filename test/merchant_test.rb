bb0c104a145d214adfd96b441838a8022d503fe4
pollockcl <pollock.clee@gmail.com>
2018-02-18 17:38:43 -0700

    modified for new design

--- test/merchant_test.rb
+++ test/merchant_test.rb
@@ -1,14 +1,19 @@
 require_relative 'test_helper'
 require './lib/merchant'
 class MerchantTest < MiniTest::Test
+  def setup
+    @attributes = { id: 1,
+                    name: 'Bobby',
+                    parent: nil }
+  end
   def test_existence
-    merchant = Merchant.new(1, 'Bobby', nil)
+    merchant = Merchant.new(@attributes)
 
     assert_instance_of Merchant, merchant
   end
 
   def test_attributes
-    merchant = Merchant.new(1, 'Bobby', nil)
+    merchant = Merchant.new(@attributes)
 
     assert_equal 1, merchant.id
     assert_equal 'Bobby', merchant.name
