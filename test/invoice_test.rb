5ad37d28b6514cac98850cefcaab692522d58306
pollockcl <pollock.clee@gmail.com>
2018-02-20 08:29:53 -0700

    slight change for the hound

--- test/invoice_test.rb
+++ test/invoice_test.rb
@@ -2,13 +2,13 @@ require_relative 'test_helper'
 require_relative '../lib/invoice.rb'
 class InvoiceTest < MiniTest::Test
   def setup
-    @attributes = {id:          6,
-                   customer_id: 7,
-                   merchant_id: 8,
-                   status:     :pending,
-                   created_at: Time.now,
-                   updated_at: Time.now,
-                   parent: nil}
+    @attributes = { id:          6,
+                    customer_id: 7,
+                    merchant_id: 8,
+                    status:     :pending,
+                    created_at: Time.now,
+                    updated_at: Time.now,
+                    parent: nil }
   end
 
   def test_existence
