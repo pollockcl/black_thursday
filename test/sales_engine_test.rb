4a19e1289aa36f8ac997bb3e3c397f85cfa6a05f
agpiermarini <piermarini.andrew@gmail.com>
2018-02-20 11:15:01 -0700

    hound fixes

--- test/sales_engine_test.rb
+++ test/sales_engine_test.rb
@@ -6,7 +6,7 @@ class SalesEngineTest < MiniTest::Test
     @files = { items:         './data/items.csv',
                merchants:     './data/merchants.csv',
                invoices:      './data/invoices.csv',
-               invoice_items: './data/invoice_items.csv'}
+               invoice_items: './data/invoice_items.csv' }
     @se             = SalesEngine.from_csv(@files)
     @merchant_repo  = @se.merchants
     @item_repo      = @se.items
