fa77183075e0a817047d0338e604011a06945bd6
pollockcl <pollock.clee@gmail.com>
2018-02-20 10:59:03 -0700

    test fits design

--- test/transaction_test.rb
+++ test/transaction_test.rb
@@ -2,7 +2,13 @@ require_relative 'test_helper'
 require_relative '../lib/transaction.rb'
 class TransactionTest < MiniTest::Test
   def setup
-    @attributes = {}
+    @attributes = { id:                          6,
+                    invoice_id:                  8,
+                    credit_card_number:          "4242424242424242",
+                    credit_card_expiration_date: "0220",
+                    result:                      "success",
+                    created_at:                  Time.now,
+                    updated_at:                  Time.now }
   end
 
   def test_existence
