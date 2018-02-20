5bfa0eac40dd7802872da485d6840d8b78216e82
pollockcl <pollock.clee@gmail.com>
2018-02-20 11:15:17 -0700

    slight changes

--- test/transaction_test.rb
+++ test/transaction_test.rb
@@ -4,9 +4,9 @@ class TransactionTest < MiniTest::Test
   def setup
     @attributes = { id:                          6,
                     invoice_id:                  8,
-                    credit_card_number:          "4242424242424242",
-                    credit_card_expiration_date: "0220",
-                    result:                      "success",
+                    credit_card_number:          '4242424242424242',
+                    credit_card_expiration_date: '0220',
+                    result:                      'success',
                     created_at:                  Time.now,
                     updated_at:                  Time.now }
   end
