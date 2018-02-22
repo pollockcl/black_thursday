8885742c3dd52a49035f60f00267fc892a451fb5
pollockcl <pollock.clee@gmail.com>
2018-02-21 17:20:30 -0700

    added tests for the data_analyst

--- test/data_analyst_test.rb
+++ test/data_analyst_test.rb
@@ -12,4 +12,28 @@ class DataAnalystTest < MiniTest::Test
 
     assert_instance_of Array, DataAnalyst.find_items(file)
   end
+
+  def test_find_transactions
+    file = './data/transactions.csv'
+
+    assert_instance_of Array, DataAnalyst.find_transactions(file)
+  end
+
+  def test_find_customers
+    file = './data/customers.csv'
+
+    assert_instance_of Array, DataAnalyst.find_customers(file)
+  end
+
+  def test_find_invoices
+    file = './data/invoices.csv'
+
+    assert_instance_of Array, DataAnalyst.find_invoices(file)
+  end
+
+  def test_find_invoice_items
+    file = './data/invoice_items.csv'
+
+    assert_instance_of Array, DataAnalyst.find_invoice_items(file)
+  end
 end
