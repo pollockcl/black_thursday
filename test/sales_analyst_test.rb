6d20c3a653a37829dce293194df7b22fce507184
agpiermarini <piermarini.andrew@gmail.com>
2018-02-22 17:34:20 -0700

    slight change to total revenue by date test to make hound happy

--- test/sales_analyst_test.rb
+++ test/sales_analyst_test.rb
@@ -105,7 +105,7 @@ class SalesAnalystTest < MiniTest::Test
 
   def test_total_revenue_by_date
     date = Time.parse("2009-02-07")
-    assert_equal 21067.77, @sa.total_revenue_by_date(date)
+    assert_equal 21_067.77, @sa.total_revenue_by_date(date)
   end
 
   def test_top_revenue_earners
