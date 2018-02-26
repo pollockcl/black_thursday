2286d3e87dad3be9988b9ec4bd4b6d92d10695e9
pollockcl <pollock.clee@gmail.com>
2018-02-26 15:50:26 -0700

    modified for new design refactor

--- test/data_analyst_test.rb
+++ test/data_analyst_test.rb
@@ -1,39 +1,9 @@
 require_relative 'test_helper'
 require './lib/data_analyst'
 class DataAnalystTest < MiniTest::Test
-  def test_find_merchants
+  def test_find_attributes
     file = './data/merchants.csv'
 
-    assert_instance_of Array, DataAnalyst.find_merchants(file)
-  end
-
-  def test_find_items
-    file = './data/items.csv'
-
-    assert_instance_of Array, DataAnalyst.find_items(file)
-  end
-
-  def test_find_transactions
-    file = './data/transactions.csv'
-
-    assert_instance_of Array, DataAnalyst.find_transactions(file)
-  end
-
-  def test_find_customers
-    file = './data/customers.csv'
-
-    assert_instance_of Array, DataAnalyst.find_customers(file)
-  end
-
-  def test_find_invoices
-    file = './data/invoices.csv'
-
-    assert_instance_of Array, DataAnalyst.find_invoices(file)
-  end
-
-  def test_find_invoice_items
-    file = './data/invoice_items.csv'
-
-    assert_instance_of Array, DataAnalyst.find_invoice_items(file)
+    assert_instance_of CSV::Table, DataAnalyst.find_attributes(file)
   end
 end
