715db87e7377a014296abbc4196b4791174e085b
pollockcl <pollock.clee@gmail.com>
2018-02-26 20:14:43 -0700

    renamed to factory

--- lib/data_analyst.rb
+++
@@ -1,7 +0,0 @@
-require 'csv'
-# DataAnalyst class. Handles csv processing
-class DataAnalyst
-  def self.find_attributes(file)
-    CSV.readlines(file, headers: true, header_converters: :symbol)
-  end
-end
