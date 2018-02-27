0d13cc18b4440b66b7947af4730e484f68662f2d
pollockcl <pollock.clee@gmail.com>
2018-02-26 20:15:03 -0700

    renamed to factory_test

--- test/data_analyst_test.rb
+++
@@ -1,9 +0,0 @@
-require_relative 'test_helper'
-require './lib/data_analyst'
-class DataAnalystTest < MiniTest::Test
-  def test_find_attributes
-    file = './data/merchants.csv'
-
-    assert_instance_of CSV::Table, DataAnalyst.find_attributes(file)
-  end
-end
